40479bc168561db82fdd6bc89ce62356c22f2647
REVANYA <anaparthirevanya2003@gmail.com>
2023-12-13 22:33:25 +0530

    maven web push

---
+++ src/main/java/com/google/maven_web/App.java
@@ -0,0 +1,13 @@
+package com.google.maven_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
